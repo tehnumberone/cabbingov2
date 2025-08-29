43aba6fb1f953ae8a13ae2749a1f7df1b5c1f5af
tehnumberone <emre_ftw@live.nl>
2025-08-29 23:02:58 +0200

    idk

--- inject-env.js
+++ inject-env.js
@@ -7,13 +7,13 @@ const envProdPath = path.join(__dirname, 'src/app/environment.prod.ts');
 const envContent = `export const environment = {
     production: true,
     firebaseConfig: {
-        apiKey: "${process.env.FIREBASE_API_KEY}",
-        authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
-        databaseURL: "${process.env.FIREBASE_DATABASE_URL}",
-        projectId: "${process.env.FIREBASE_PROJECT_ID}",
-        storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
-        messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
-        appId: "${process.env.FIREBASE_APP_ID}"
+        apiKey: "${process.env.NG_ENV_FIREBASE_API_KEY}",
+        authDomain: "${process.env.NG_ENV_FIREBASE_AUTH_DOMAIN}",
+        databaseURL: "${process.env.NG_ENV_FIREBASE_DATABASE_URL}",
+        projectId: "${process.env.NG_ENV_FIREBASE_PROJECT_ID}",
+        storageBucket: "${process.env.NG_ENV_FIREBASE_STORAGE_BUCKET}",
+        messagingSenderId: "${process.env.NG_ENV_FIREBASE_MESSAGING_SENDER_ID}",
+        appId: "${process.env.NG_ENV_FIREBASE_APP_ID}"
     }
 };`;
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay, withNoHttpTransferCache } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp, getApps, getApp } from '@angular/fire/app';
-import { provideDatabase, getDatabase } from '@angular/fire/database'; // Changed from Firestore
+import { provideDatabase, getDatabase } from '@angular/fire/database';
 
 import { environment } from './environment';
 
