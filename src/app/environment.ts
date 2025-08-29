4a637b9bcc2c51c99e271a1d2d1a185744de1afc
emre secuwave <emre_ftw@live.nl>
2025-08-29 22:01:31 +0200

    Update environment.ts

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: process.env['NG_APP_FIREBASE_API_KEY'] || "AIzaSyB2e36wu47-6DOc2_rAuHfiFTq9vm6GBD0",
-        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || "cabbingo-db.firebaseapp.com",
-        databaseURL: process.env['NG_APP_FIREBASE_DATABASE_URL'] || "https://cabbingo-db-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || "cabbingo-db",
-        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || "cabbingo-db.firebasestorage.app",
-        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || "684304316440",
-        appId: process.env['NG_APP_FIREBASE_APP_ID'] || "1:684304316440:web:0660b0d72a68752cadeb6d"
+        apiKey: process.env['NG_APP_FIREBASE_API_KEY'] ,
+        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] ,
+        databaseURL: process.env['NG_APP_FIREBASE_DATABASE_URL'],
+        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
+        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
+        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
+        appId: process.env['NG_APP_FIREBASE_APP_ID']
     }
 };
