adaf3b5be150c38ce145b149a279f5387f19d1f2
tehnumberone <emre_ftw@live.nl>
2025-08-29 22:14:08 +0200

    Update environment.prod.ts

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: true,
     firebaseConfig: {
-        apiKey: process.env['NG_APP_FIREBASE_API_KEY'] || "",
-        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || "",
-        databaseURL: process.env['NG_APP_FIREBASE_DATABASE_URL'] || "",
-        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || "",
-        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || "",
-        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || "",
-        appId: process.env['NG_APP_FIREBASE_APP_ID'] || ""
+        apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
+        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
+        databaseURL: process.env['NG_APP_FIREBASE_DATABASE_URL'],
+        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] ,
+        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
+        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
+        appId: process.env['NG_APP_FIREBASE_APP_ID']
     }
 };
